34473e1908da50bba105d57c24f70bf14e653b16
Your NameAnthony <anthonyclarence.goo@gmail.com>
2024-07-11 09:39:30 -0700

    Mujoco Python Simulation Learning. Figuring out mjdata

--- SRC/RL Learning/MujocoBasic.py
+++ SRC/RL Learning/MujocoBasic.py
@@ -1,7 +1,13 @@
 # This code is pulling the skydio xml scene and going through a single step
-
 import mujoco
 import os
+
+import gymnasium as gym
+from gymnasium import spaces
+import random
+import time
+
+import matplotlib.pyplot as plt
 
 Path_Total = os.getcwd()
 
@@ -18,6 +24,24 @@ data = mujoco.MjData(model)
 # Step through the model once
 mujoco.mj_step(model,data)
 
+Timevec = []
+PosVec = []
+# Step through the model once
+for i in range(1000):
+    mujoco.mj_step(model,data)
+    Timevec.append(data.time)
+    PosVec.append(data.qpos[2])
+    print('time ', data.time, 'position ', data.qpos[2])
+
+# NOTES
+# position and velocity vector are organized as follows
+# x y z , quaternion
 
 
-print("Checkpoint")
+plt.style.use('_mpl-gallery')
+fig, ax = plt.subplots()
+ax.plot(Timevec,PosVec, linewidth = 2.0)
+ax.set(xlim = (0,2), ylim = (0, 0.1))
+
+plt.show()
+print("End")
